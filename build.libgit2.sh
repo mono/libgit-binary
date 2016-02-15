d8b2acad2fdb0f6cc8823f8dc969576f1962f6a2
Marius Ungureanu <marius.ungureanu@xamarin.com>
2016-02-15 22:34:09 +0200

    Revert "bump the world and enable NSEC checks."
    This reverts commit 7618d373ce667010cd00b133ee1c14e1e6a57b98.

--- build.libgit2.sh
+++ build.libgit2.sh
@@ -85,7 +85,6 @@ then
 	      -DLIBSSH2_LIBRARY_DIRS="$CURDIR/external/libssh2/build/src" \
 	      -DSSH_LIBRARIES="ssh2" \
 	      -DHAVE_LIBSSH2_MEMORY_CREDENTIALS=TRUE \
-	      -DUSE_NSEC=ON \
 	      ..
 else
 	cmake -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
